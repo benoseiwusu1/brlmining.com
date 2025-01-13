766b5538050d6a34bfa09c2e713a2495a089c16e
ranselorm <ranselorm@outlook.com>
2025-01-13 23:12:48 +0000

    Refactor investors page

--- app/investors/page.tsx
+++ app/investors/page.tsx
@@ -12,7 +12,12 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { Card, CardContent } from "@/components/ui/card";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { spawn } from "child_process";
 
 const InvestorCentreOverview: React.FC = () => {
@@ -68,7 +73,8 @@ const InvestorCentreOverview: React.FC = () => {
 };
 
 const Investors: React.FC = () => {
-  const [selectedCard, setSelectedCard] = useState(null);
+  const [selectedCard, setSelectedCard] = useState<any>(null);
+  console.log(selectedCard);
 
   const cards = [
     {
@@ -97,6 +103,7 @@ const Investors: React.FC = () => {
           ],
         },
       ],
+      image: "/images/bg.jpg",
     },
     {
       title: "Strong Economic Potential",
@@ -110,6 +117,7 @@ const Investors: React.FC = () => {
           ],
         },
       ],
+      image: "/images/hero2.jpg",
     },
     {
       title: "Exploration Upside",
@@ -118,25 +126,43 @@ const Investors: React.FC = () => {
           text: "The Sewum Gold Project shows considerable potential for growth:",
           subPoints: [
             "Depth Potential: Resource remains open at depth with high-grade shoots offering promising underground targets.",
-            "Surface Exploration: Historical surface exploration yielded encouraging drill results, indicating significant mineralization at depth and along strike.",
+            "Surface Exploration: Historical surface exploration yielded encouraging drill results, indicating significant mineralization at depth and along strike. Highlights from recent drill holes include: 8 meters at 0.43 g/t Au from 4 to 12 meters (Hole SWRC085), 1m at 0.66 g/t Au from 0 to 1m (Hole SWRC087), 2m at 0.38 g/t Au from 60 to 62m (SWRC089).",
           ],
         },
       ],
+      image: "/images/hero1.jpg",
+    },
+    {
+      title: "Proximity to Key Resources",
+      points: [
+        {
+          text: "Located 25 km from Enchi, which provides logistical support and enhances operational efficiency.",
+        },
+      ],
+      image: "/images/bg.jpg",
+    },
+    {
+      title: "Experienced Management Team",
+      points: [
+        {
+          text: "BRL’s leadership team brings extensive experience in advancing mining projects from exploration to production, with a proven track record across Africa.",
+        },
+      ],
+      image: "/images/hero3.jpg",
     },
   ];
 
   return (
-    <div className="bg-deep">
+    <div className="">
       <Hero
         title="Investors"
         background="/images/report-bg.jpg"
         description="A Strategic Investment in a Prolific Gold Belt and Emerging Gold Mining District. Breakthrough Resources Limited (BRL) presents a unique investment opportunity through our flagship asset, the Sewum Gold Project. Located in the Aowin district of Ghana’s Western North Region, this advanced-stage exploration project is positioned within one of West Africa’s most prolific gold belts. The Sewum Gold Project not only overlaps the historic high-grade underground mine but also sits in a region celebrated for its substantial gold deposits."
       />
+      <h3 className="text-4xl mx-auto max-w-3xl text-center my-16 uppercase">
+        Why Invest in Breakthrough Resources Limited?
+      </h3>
       <div className="container mx-auto px-4 md:px-20">
-        <h3 className="text-4xl mx-auto max-w-xl text-center my-12">
-          Why Invest in Breakthrough Resources Limited?
-        </h3>
-
         <Carousel>
           <CarouselContent>
             {cards.map((card, index) => (
@@ -152,41 +178,66 @@ const Investors: React.FC = () => {
                     />
 
                     {/* Semi-Transparent Overlay */}
-                    <div className="absolute inset-0 bg-black bg-opacity-75 group-hover:bg-opacity-80 z-10 transition duration-150" />
+                    <div className="absolute inset-0 bg-black bg-opacity-80 group-hover:bg-opacity-85 z-10 transition duration-150" />
 
                     {/* Card Content */}
-                    <CardContent className="relative z-20 flex flex-col items-center justify-center gap-y-4 py-4 px-8 h-56">
-                      <span className="text-xl font-semibold text-white text-center w-[250px] uppercase">
-                        {card.title}
-                      </span>
-                      {card.points.map(({ text }) => (
-                        <span className="text-white text-center">
-                          {text.slice(0, 60)}...
+                    <CardContent className="relative z-20 flex flex-col items-center justify-between h-64 p-4">
+                      <div className="flex flex-col items-center">
+                        <span className="text-xl font-semibold text-white text-center w-[300px] uppercase text-bold ">
+                          {card.title}
                         </span>
-                      ))}
-                    </CardContent>
+                        {card.points.map(({ text }, index) => (
+                          <span
+                            className="text-white text-center mt-2"
+                            key={index}
+                          >
+                            {text.slice(0, 150)} ...
+                          </span>
+                        ))}
+                      </div>
 
-                    {/* Action Button */}
-                    <div className="absolute bottom-4 left-4 z-30">
                       <Dialog>
-                        {/* <DialogTrigger asChild>
+                        <DialogTrigger asChild>
                           <button
-                            className="bg-white px-4 py-2 text-sm font-semibold rounded shadow hover:bg-gray-100"
+                            className="bg-transparent text-white border border-primary px-6 py-2 text-xs font-semibold rounded shadow uppercase"
                             onClick={() => setSelectedCard(card)}
                           >
                             Learn More
                           </button>
-                        </DialogTrigger> */}
-                        <DialogContent>
-                          <h3 className="text-xl font-bold">
+                        </DialogTrigger>
+                        <DialogContent className="md:max-w-[50vw]">
+                          <DialogTitle className="text-xl uppercase mb-4">
                             {selectedCard?.title}
-                          </h3>
-                          <p className="mt-2 text-sm">
-                            {selectedCard?.description}
-                          </p>
+                          </DialogTitle>
+
+                          {/* Iterate through points */}
+                          {selectedCard?.points.map(
+                            (point: any, index: number) => (
+                              <div key={index} className="mb-4">
+                                <p className="text-base mb-2 leading-relaxed">
+                                  {point.text}
+                                </p>
+                                {/* Iterate through subPoints */}
+                                {point.subPoints && (
+                                  <ul className="list-disc pl-5 text-base">
+                                    {point.subPoints.map(
+                                      (subPoint: string, subIndex: number) => (
+                                        <li
+                                          key={subIndex}
+                                          className="mb-1 leading-relaxed"
+                                        >
+                                          {subPoint}
+                                        </li>
+                                      )
+                                    )}
+                                  </ul>
+                                )}
+                              </div>
+                            )
+                          )}
                         </DialogContent>
                       </Dialog>
-                    </div>
+                    </CardContent>
                   </Card>
                 </div>
               </CarouselItem>
@@ -196,7 +247,17 @@ const Investors: React.FC = () => {
           <CarouselNext />
         </Carousel>
       </div>
-      <InvestorCentreOverview />
+
+      {/* <InvestorCentreOverview /> */}
+      <div className="bg-primary w-full h-56 mt-12 p-6 flex items-center justify-center">
+        <div className="container mx-auto px-4 md:px-20 text-white">
+          <h3 className="text-2xl text-center">
+            Breakthrough Resources Limited is well positioned to leverage the
+            Sewum Gold Project's strategic assets and exploration potential to
+            deliver substantial returns.
+          </h3>
+        </div>
+      </div>
       <RssFeed />
     </div>
   );
