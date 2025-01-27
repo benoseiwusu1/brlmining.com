4a0adaa7fe3fa99213e871cf4fb0afbc04402282
ranselorm <ranselorm@outlook.com>
2025-01-27 16:31:47 +0000

    Change contact hero image

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,7 +6,10 @@ import ContactForm from "@/components/ContactForm";
 const Contact: React.FC = () => {
   return (
     <section>
-      <Hero title="Contact" background="/images/contact-bg.jpg" />
+      <Hero
+        title="Contact"
+        background="https://images.unsplash.com/photo-1560408677-b4af8a53052e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
       <ContactDetails />
       <ContactForm />
     </section>
