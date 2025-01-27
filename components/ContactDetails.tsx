0891999ed488af04ba2e3b8b46d124dbbdf0f0a7
ranselorm <ranselorm@outlook.com>
2025-01-27 16:39:56 +0000

    Modify code

--- components/ContactDetails.tsx
+++ components/ContactDetails.tsx
@@ -5,7 +5,7 @@ const ContactDetails: React.FC = () => {
     <div className="bg-gray-100 flex flex-col md:flex-row items-center md:items-center">
       {/* Left Image */}
       <div className="w-full md:w-1/2 mb-6 md:mb-0 h-[600px]">
-        <iframe
+        {/* <iframe
           src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d430.5729139967408!2d-0.14494827738223284!3d5.637106408538737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sgh!4v1736755559487!5m2!1sen!2sgh"
           width="100%"
           height="100%"
@@ -13,7 +13,12 @@ const ContactDetails: React.FC = () => {
           // referrerpolicy="no-referrer-when-downgrade"
           style={{ border: 0 }}
           allowFullScreen={false}
-        ></iframe>
+        ></iframe> */}
+        <img
+          src="https://images.unsplash.com/photo-1605436247078-f0ef43ee8d5c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fHw%3D"
+          alt=""
+          className="w-full h-full object-cover"
+        />
       </div>
 
       {/* Right Contact Details */}
