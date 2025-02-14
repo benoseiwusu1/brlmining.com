f9893d0da3a3c11420336bf6fb4e2393aa3367e1
ranselorm <ranselorm@outlook.com>
2025-02-14 21:18:55 +0000

    Refactor

--- components/Info.tsx
+++ components/Info.tsx
@@ -9,24 +9,6 @@ const Info: React.FC = () => {
       style={{ backgroundImage: "url('/images/world.jpg')" }}
     >
       <div className="flex flex-col md:flex-row justify-center items-center gap-8 py-4 md:py-16 px-4 md:px-24">
-        {/* <div className="bg-primary text-white p-6 md:w-1/2 rounded">
-          <div className="flex items-center mb-4">
-            <Icon icon="mdi:finance" className="text-3xl mr-2" />
-            <h2 className="text-3xl font-bold">Investor Centre</h2>
-          </div>
-          <p className="my-6">
-            Our goal is to generate wealth through the discovery of a new gold
-            deposit.
-          </p>
-          <Link
-            href="/investorS"
-            className="inline-block mt-4 px-6 py-3 border border-white text-white rounded-full text-sm"
-          >
-            INVESTOR INFO
-          </Link>
-        </div> */}
-
-        {/* Company Card */}
         <div className="bg-primary text-white p-6 md:w-1/2 rounded-lg shadow-lg">
           <div className="flex items-center mb-4">
             <Icon
