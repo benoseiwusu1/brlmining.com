508735bdc61fe311d105f7972b9233e1fd6b8bd2
ranselorm <ranselorm@outlook.com>
2025-02-14 21:20:02 +0000

    Refactor

--- components/ClientProvider.tsx
+++ components/ClientProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function ClientProvider({
   children,
