a2de81650d38fdae3f944d45bcdc483b895c4432
ranselorm <ranselorm@outlook.com>
2025-02-17 14:53:35 +0000

    Change background image

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
     <section>
       <Hero
         title="About the Company"
-        background="/images/about.jpg"
+        background="/images/aboutt.jpg"
         description="Breakthrough Resources Ltd. is a forward-thinking junior exploration and development company dedicated to unlocking the full potential of our flagship Sewum Gold Project. Located in the Aowin district of Ghana's Western North Region, the Sewum Gold Project spans approximately 0.63 square kilometers and is situated in one of West Africa's most prolific gold mining areas."
       />
       <About />
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,4 +1,3 @@
-import React, { useState, useRef, useEffect } from "react";
 import { Icon } from "@iconify/react/dist/iconify.js";
 
 const explore = [
